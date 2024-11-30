76204af4376627ae80581211f2bf2ceeb40ba18d
ISylkin <ISylkin@croc.ru>
2024-12-01 00:56:36 +0300

    #fix: vpn name

--- src/modules/qrCode/utils/getUrl.ts
+++ src/modules/qrCode/utils/getUrl.ts
@@ -8,4 +8,4 @@ export const getUrl = ({
 	sid,
 	country,
 }: IURLData): string =>
-	`vless://${clientId}@${ip}:443/?type=tcp&encryption=none&flow=xtls-rprx-vision&sni=${sni}&alpn=h2&fp=chrome&security=reality&pbk=${pbk}&sid=${sid}&packetEncoding=xudp#${country}_test`;
+	`vless://${clientId}@${ip}:443/?type=tcp&encryption=none&flow=xtls-rprx-vision&sni=${sni}&alpn=h2&fp=chrome&security=reality&pbk=${pbk}&sid=${sid}&packetEncoding=xudp#${country}`;
